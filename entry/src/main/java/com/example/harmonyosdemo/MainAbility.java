9d6fac021054a09efedf6251d67f31c97ebaf3b9
luckly <17752170152@163.com>
2022-01-21 12:35:24 +0800

    redeme

--- entry/src/main/java/com/example/harmonyosdemo/MainAbility.java
+++ entry/src/main/java/com/example/harmonyosdemo/MainAbility.java
@@ -1,6 +1,7 @@
 package com.example.harmonyosdemo;
 
 import com.example.harmonyosdemo.slice.MainAbilitySlice;
+import com.example.harmonyosdemo.slice.SecondAbilitySlice;
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.content.Intent;
 
@@ -8,6 +9,9 @@ public class MainAbility extends Ability {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
+        //默认显示
         super.setMainRoute(MainAbilitySlice.class.getName());
+        //配置路由规则显示
+        addActionRoute( "action.second", SecondAbilitySlice.class.getName());
     }
 }
